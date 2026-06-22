4c8e390c063088ee701f5bd6e49269cbee263e8e
José Santos <wandersonjose75@gmail.com>
2026-06-22 12:27:26 -0300

    Added and gate project

---
+++ Basics/AND/tb.v
@@ -0,0 +1,21 @@
+module tb;
+
+reg a;
+reg b;
+wire y;
+
+and_gate dut(.a(a), .b(b), .y(y));
+
+initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0,tb);
+
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+
+    $finish;
+end
+
+endmodule
