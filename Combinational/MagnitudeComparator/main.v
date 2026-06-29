1b46ba216c7a3ed05949eb55e98e2af9e8e70220
José Wanderson <wandersonjose75@gmail.com>
2026-06-29 20:02:39 -0300

    [MAGCMP] Using relational operators

--- Combinational/MagnitudeComparator/main.v
+++ Combinational/MagnitudeComparator/main.v
@@ -13,8 +13,12 @@ module magcmp(a, b, aeqb, agtb, altb);
     assign x2 = ~(a[2] ^ b[2]);
     assign x3 = ~(a[3] ^ b[3]);
 
-    assign aeqb = x3 & x2 & x1 & x0;
-    assign agtb = a[3] & (~b[3]) | x3 & a[2] & (~b[2]) | x3 & x2 & a[1] & (~b[1]) | x3 & x2 & x1 & a[0] & (~b[0]);
-    assign altb = (~a[3]) & b[3] | x3 & (~a[2]) & b[2] | x3 & x2 & (~a[1]) & b[1] | x3 & x2 & x1 & (~a[0]) & b[0];
+    // assign aeqb = x3 & x2 & x1 & x0;
+    // assign agtb = a[3] & (~b[3]) | x3 & a[2] & (~b[2]) | x3 & x2 & a[1] & (~b[1]) | x3 & x2 & x1 & a[0] & (~b[0]);
+    // assign altb = (~a[3]) & b[3] | x3 & (~a[2]) & b[2] | x3 & x2 & (~a[1]) & b[1] | x3 & x2 & x1 & (~a[0]) & b[0];
+    
+    assign aeqb = a == b;
+    assign agtb = a > b;
+    assign altb = a < b;
 
 endmodule
