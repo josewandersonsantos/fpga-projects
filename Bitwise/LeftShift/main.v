0ea0e6048738f9a144b3af0bb2bc07d96403074f
José Wanderson <wandersonjose75@gmail.com>
2026-06-29 14:06:44 -0300

    Added left shift project

---
+++ Bitwise/LeftShift/main.v
@@ -0,0 +1,13 @@
+/*
+ * Left Shift
+ * a 8bit, sh 3bit
+ */
+`default_nettype none
+module leftshift(a, sh, o);
+    input [7:0] a;
+    input [3:0] sh;
+    output [7:0] o;
+
+    assign o = a << sh;
+
+endmodule
