e2eb5da9e92349d18e87cec408eb9809de943a9c
José Santos <wandersonjose75@gmail.com>
2026-07-02 13:56:18 -0300

    [CPU8BIT] Added test firmware

---
+++ Advanced/CPU8Bit/firmwares/main.s
@@ -0,0 +1,16 @@
+$0 |   0D  // LDA [$D]   Load A with the value at address $D
+$1 |   1E  // ADD [$E]   Add the value at address $E to A
+$2 |   2F  // SUB [$F]   Subtract the value at address $F from A
+$3 |   F0  // HLT        Stop execution
+$4 |   00
+$5 |   00
+$6 |   00
+$7 |   00
+$8 |   00 
+$9 |   00 
+$A |   00 
+$B |   00
+$C |   00
+$D |   03  // Data: 3
+$E |   04  // Data: 4
+$F |   02  // Data: 2
