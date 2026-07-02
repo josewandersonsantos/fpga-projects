5682a5ff7f754caa45c6dba6846971811256b284
José Santos <wandersonjose75@gmail.com>
2026-07-02 13:57:22 -0300

    [CPU8BIT] Added test bench

---
+++ Advanced/CPU8Bit/tb.v
@@ -0,0 +1,44 @@
+module main_tb;
+reg clk = 0, rst = 1;
+
+main uut(.clk_in(clk), .rst_in(rst));
+
+initial begin
+    $dumpfile("main.vcd");
+    $dumpvars(0, main_tb);
+    rst = 1; #20 rst = 0;
+    #2000;
+
+    $finish;
+end
+
+always #5 clk = ~clk;
+
+always @(posedge clk) begin
+    $display(
+        "T=%0t S=%0d PC=%h MEMADDR=%h BUS=%h IRREG=%h IROUT=%h A=%h B=%h | PC_EN=%b PC_INC=%b MEM_EN=%b MEM_LD=%b IR_EN=%b IR_LD=%b A_EN=%b A_LD=%b B_LD=%b ADD_EN=%b SUB=%b HLT=%b",
+        $time,
+        uut.uut_controller.state,
+        uut.uut_pc.pc,
+        uut.uut_mem.addr,
+        uut.bus,        
+        uut.uut_ir.ir,
+        uut.ir_out,
+        uut.a_out,
+        uut.b_out,
+        uut.pc_en,
+        uut.pc_inc,
+        uut.mem_en,
+        uut.mem_ld,
+        uut.ir_en,
+        uut.ir_ld,
+        uut.a_en,
+        uut.a_ld,
+        uut.b_ld,
+        uut.addsub_en,
+        uut.addsub_sub_flag,
+        uut.hlt
+    );
+end
+
+endmodule
